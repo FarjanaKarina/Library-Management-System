98b6e76e670eddfee9ee604f0af136a5760d84c1
AowsafAnzum <134544926+AowsafAnzum@users.noreply.github.com>
2025-10-20 13:52:49 +0600

    new commit

---
+++ DataModels/MemberModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace LibraryManagementSystem.Models
+{
+    public class Member
+    {
+        [Key]
+        public int MemberId { get; set; }
+
+        [Required(ErrorMessage = "Member name is required")]
+        [StringLength(100)]
+        public string MemberName { get; set; }
+
+        [Required(ErrorMessage = "Member type is required")]
+        [StringLength(50)]
+        public string MemberType { get; set; } // e.g., Student, Teacher, Public
+
+        [StringLength(150)]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string ContactInfo { get; set; } // can store email or phone
+
+        [Required]
+        public string Status { get; set; } = "Active"; // Active / Inactive
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Expiry Date")]
+        public DateTime ExpiryDate { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Registration Date")]
+        public DateTime RegistrationDate { get; set; } = DateTime.Now;
+    }
+}
